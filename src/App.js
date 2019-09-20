e9008dbab36d86c5adc2a96b37b2a73a51e804c8
evansibok <evansibok@gmail.com>
2019-09-20 15:14:33 +0100

    set up axios request to the api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header.js";
 import TabNav from "./components/TabNav";
+import axios from "axios";
 
 
 export default function App() {
+
+  const charApi = "https://rickandmortyapi.com/api/character/";
+  const [ character, setCharacter ] = useState([]);
+
+  useEffect(() => {
+
+    axios.get(charApi)
+      .then(res => {
+        setCharacter(res.data.results)
+      })
+      .catch(err => {
+        return err
+      })
+  }, []);
+
   return (
     <main>
       <Header />
