bd912684d42963a18ddf0fcfd2419b2eae2ff468
evansibok <evansibok@gmail.com>
2019-10-06 08:07:18 +0100

    added change handler to the searchform component

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 export default function SearchForm() {
+
+  const changeHandler = e => {
+    setSearchValue(e.target.value)
+    console.log(e.target.value)
+  }
+
  
   return (
     <section className="search-form">
-      <input className="newStyle" type="text" placeholder="Search..." />
+      <input className="newStyle" type="search" placeholder="Search..." onChange={e => changeHandler(e)}/>
     </section>
   );
 }
